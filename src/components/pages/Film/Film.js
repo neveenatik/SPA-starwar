949113ad7c709329d657d0d72cc56c7a60fc8e6e
natik <mac@MACs-MacBook.local>
2019-04-27 01:06:20 +0200

    debugged film component and added loading state

--- src/components/pages/Film/Film.js
+++ src/components/pages/Film/Film.js
@@ -3,26 +3,41 @@ import {connect} from 'react-redux';
 import {fetchFilm} from '../../../actions/filmsAction';
 import {Link} from 'react-router-dom';
 
+import './Film.scss';
+
 class Film extends Component {
     componentDidMount() {
-        fetchFilm(this.props.match.params.id);
+        this.props.fetchFilm(this.props.match.params.id);
     }
   render() {
       const film = this.props.film;
     return (
-      <div>
+      <div className='container'>
         <Link to='/'>Back to movies search</Link>
+        {
+            this.props.status.loading ? 
+            <div className='loading'> loading </div> 
+            : 
+        <div className='details'>
         <h2 className="film-title">{film.title}</h2>
         <p><span className='bold-start'>Director: </span>{film.director}</p>
         <p><span className='bold-start'>Producer: </span>{film.producer}</p>
         <p><span className='bold-start'>Release Date: </span>{film.release_date}</p>
+        <p className='bold-start'>Opening crawl:</p>
+        <p>"{film.opening_crawl}"</p>
+        </div>
+        }
       </div>
     )
   }
 }
 
 const mapStateToProps = state => ({
-    film: state.selectedFilm
+    film: state.selectedFilm,
+    status: state.status
 })
 
-export default connect(mapStateToProps, {fetchFilm})(Film);
+export default connect(
+    mapStateToProps, 
+    {fetchFilm}
+)(Film);

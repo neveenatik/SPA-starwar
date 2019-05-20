ab925c2aa0331c3790e97d4de48153116a793909
Funatik <funatic@MacBookAir.local>
2019-05-20 02:50:58 +0200

    Updates

---
+++ precache-manifest.266acc70672a58e4769cdbd4a870c17b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0019d1e53b8d478ba8606822d240aa33",
+    "url": "/create-react-app-skeleton/index.html"
+  },
+  {
+    "revision": "1ed6daf2393546efaa98",
+    "url": "/create-react-app-skeleton/static/css/main.e2793f1b.chunk.css"
+  },
+  {
+    "revision": "da88f3b3e296c2ad5810",
+    "url": "/create-react-app-skeleton/static/js/2.ea16d46d.chunk.js"
+  },
+  {
+    "revision": "1ed6daf2393546efaa98",
+    "url": "/create-react-app-skeleton/static/js/main.44184756.chunk.js"
+  },
+  {
+    "revision": "32ef77dc2ba45cda42e6",
+    "url": "/create-react-app-skeleton/static/js/runtime~main.881666c1.js"
+  },
+  {
+    "revision": "845737f583bc7b5d3256ef7c3fc8a135",
+    "url": "/create-react-app-skeleton/static/media/profile-avatar.845737f5.png"
+  }
+]);

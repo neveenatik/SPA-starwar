67ab5370a9043493b7a9fe21cdf098c060e1bc27
Funatik <funatic@MacBookAir.local>
2019-05-20 03:00:39 +0200

    Added full api path, I don't know why proxy in the package.json is not used in production

--- src/utils/endpoint.js
+++ src/utils/endpoint.js
@@ -2,26 +2,26 @@ import axios from 'axios';
 
 export function fetchAllFilms(page) {
     if(page) {
-        return axios.get(`/api/films/?page=${page}`);
+        return axios.get(`https://swapi.co/api/films/?page=${page}`);
     }
-    return axios.get(`/api/films`);
+    return axios.get(`https://swapi.co/api/films`);
 }
 
 export function updateFilms(input) {
-    return axios.get(`/api/films?search=${input}`);
+    return axios.get(`https://swapi.co/api/films?search=${input}`);
 }
 
 export function fetchSingleFilm(id) {
-    return axios.get(`/api/films/${id}`);
+    return axios.get(`https://swapi.co/api/films/${id}`);
 }
 
 export function fetchActors(page) {
     if(page) {
-        return axios.get(`/api/people/?page=${page}`);
+        return axios.get(`https://swapi.co/api/people/?page=${page}`);
     }
-    return axios.get(`/api/people`);
+    return axios.get(`https://swapi.co/api/people`);
 }
 
 export function updateActors(input) {
-    return axios.get(`/api/people?search=${input}`);
+    return axios.get(`https://swapi.co/api/people?search=${input}`);
 }
